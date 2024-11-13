30feec586fb7faff1761c5ad0df1b559c643090b
PINnew <pinnew@ya.ru>
2024-11-13 06:20:32 +0400

    add code new

---
+++ two_title/myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def data(request):
+    return HttpResponse("<h1>Это страница Data</h1><p>Содержимое страницы Data.</p>")
+
+def test(request):
+    return HttpResponse("<h1>Это страница Test</h1><p>Содержимое страницы Test.</p>")
