b03fc0781acf26e4a6040ad36bcc25ffe4cd2297
PINnew <pinnew@ya.ru>
2024-11-14 16:59:19 +0400

    add code new

--- two_title/myapp/urls.py
+++ two_title/myapp/urls.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.data),
-    path('test', views.test),
+    path('', views.test),
 ]
--- two_title/myapp/views.py
+++ two_title/myapp/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 from django.http import HttpResponse
-
 
 def data(request):
     return HttpResponse("<h1>Это страница Data</h1><p>Содержимое страницы Data.</p>")
